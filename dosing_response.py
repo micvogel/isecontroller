f3c8a7f65071f0f7dddd324dcb5b386ee6687091
root <michaelvogel93@hotmail.com>
2020-07-07 16:30:51 +0200

    10810 Versuche

--- dosing_response.py
+++ dosing_response.py
@@ -10,11 +10,15 @@ for i in range(5):
     print(5-i)
     time.sleep(1)
     
-#Dose 8 ml
-new_dosingpump.dose(30,40)
-time.sleep(10)
 #Dose 10 ml
-# new_dosingpump.dose(10,5)
+new_dosingpump.dose(10.57,100)
+time.sleep(150)
+#Dose 8 ml
+new_dosingpump.dose(8.7,100)
+time.sleep(150)
+#Dose 10 ml
+new_dosingpump.dose(10.57,100)
+time.sleep(150)
 
 
 
